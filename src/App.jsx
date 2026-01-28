b0501bace91d5cf04d4aeb691ad2a4a79ee04d52
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2026-01-28 16:59:04 -0300

    algorithm selector created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import "./app.css"
+import algorithms from "./consts/algorithms"
+
 function App() {
   return (
     <>
+      <form>
+        <select>
+          {algorithms.map((algsObject, index) => (
+              <optgroup label={algsObject.category} key={index}>
+                {algsObject.algs.map((alg, index) => (
+                  <option value={alg.label} key={index}>{alg.label}</option>
+                ))}
+              </optgroup>
+          ))}
+        </select>
+        <button>Escolher algoritimo</button>
+      </form>
     </>
   )
 }
---
+++ src/consts/algorithms.js
@@ -0,0 +1,34 @@
+const algorithms = [
+    {
+        category: "sort",
+        algs: [
+            {
+                label: "quick sort",
+                element: "",
+            },
+            {
+                label: "bubble sort",
+                element: "",
+            },
+            {
+                label: "merge sort",
+                element: "",
+            },
+        ],
+    },
+    {
+        category: "search",
+        algs: [
+            {
+                label: "binary search",
+                element: "",
+            },
+            {
+                label: "linear search",
+                element: "",
+            },
+        ],
+    },
+];
+
+export default algorithms;
